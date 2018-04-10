75b6f9f4a0e002016a41fc8bca7426c1624526ce
dmaflab <danielmaflab@hotmail.com>
2018-04-10 10:00:16 -0500

    comienzo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCEg94DGJu4Xf_fGua4fb52ysYK0z7BhA8",
-    authDomain: "angular5-crud-b366c.firebaseapp.com",
-    databaseURL: "https://angular5-crud-b366c.firebaseio.com",
-    projectId: "angular5-crud-b366c",
-    storageBucket: "angular5-crud-b366c.appspot.com",
-    messagingSenderId: "1032386966231"
+
+    apiKey: 'AIzaSyAfns_Gt0yUtM4VPumEXo7oD_7h3HNXnjI',
+    authDomain: 'angular-crud-firebase-75cf9.firebaseapp.com',
+    databaseURL: 'https://angular-crud-firebase-75cf9.firebaseio.com',
+    projectId: 'angular-crud-firebase-75cf9',
+    storageBucket: '',
+    messagingSenderId: '826421560086'
+
   }
 };
